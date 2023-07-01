03657e38be8704b6f1d483636ce55394eb97f530
KrouriB <brice.krouri@gmail.com>
2023-07-01 23:26:18 +0200

    add manager

--- controller/ShowController.php
+++ controller/ShowController.php
@@ -6,6 +6,10 @@
     use App\AbstractController;
     use App\ControllerInterface;
     // use Model\Managers\""Manager;
+    use Model\Managers\AnneauManager;
+    use Model\Managers\EmblemManager;
+    use Model\Managers\HerosManager;
+    use Model\Managers\QttManager;
     
     class ShowController extends AbstractController implements ControllerInterface{
 
