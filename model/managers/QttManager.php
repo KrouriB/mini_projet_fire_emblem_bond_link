144d05d20741285b91f6bde3478a77b55bec1c43
KrouriB <brice.krouri@gmail.com>
2023-07-02 22:08:39 +0200

    change the nome of methods

--- model/managers/QttManager.php
+++ model/managers/QttManager.php
@@ -14,7 +14,7 @@
             parent::connect();
         }
 
-        public function allRing(){
+        public function allTheRing(){
             $sql="
                 SELECT h.nom , a.lettre , q.qtt
                 FROM qtt q
@@ -27,13 +27,13 @@
             );
         }
 
-        public function ringByRarity($id){
+        public function allRiningByRarity($id){
             $sql="
-            SELECT h.nom , q.qtt
-            FROM qtt q
-            INNER JOIN heros h ON q.heros_id = h.id_heros
-            INNER JOIN anneau a ON q.anneau_id = a.id_anneau
-            WHERE a.id_anneau = :id";
+                SELECT h.nom , q.qtt
+                FROM qtt q
+                INNER JOIN heros h ON q.heros_id = h.id_heros
+                INNER JOIN anneau a ON q.anneau_id = a.id_anneau
+                WHERE a.id_anneau = :id";
 
             return $this->getMultipleResults(
                     DAO::select($sql,['id'=>$id]),
@@ -41,13 +41,13 @@
             );
         }
 
-        public function ringByEmplacement($id){
+        public function allRingByEmplacement($id){
             $sql="
-            SELECT h.nom , q.qtt
-            FROM qtt q
-            INNER JOIN heros h ON q.heros_id = h.id_heros
-            INNER JOIN anneau a ON q.anneau_id = a.id_anneau
-            WHERE h.emplacement = :id";
+                SELECT h.nom , q.qtt
+                FROM qtt q
+                INNER JOIN heros h ON q.heros_id = h.id_heros
+                INNER JOIN anneau a ON q.anneau_id = a.id_anneau
+                WHERE h.emplacement = :id";
 
             return $this->getMultipleResults(
                     DAO::select($sql,['id'=>$id]),
@@ -55,14 +55,14 @@
             );
         }
 
-        public function ringByEmplacement($id){
+        public function allRingByEmplacement($id){
             $sql="
-            SELECT h.nom , a.lettre , q.qtt
-            FROM qtt q
-            INNER JOIN heros h ON q.heros_id = h.id_heros
-            INNER JOIN anneau a ON q.anneau_id = a.id_anneau
-            INNER JOIN emblem e ON h.emblem_id = e.id_emblem
-            WHERE e.id_emblem = :id";
+                SELECT h.nom , a.lettre , q.qtt
+                FROM qtt q
+                INNER JOIN heros h ON q.heros_id = h.id_heros
+                INNER JOIN anneau a ON q.anneau_id = a.id_anneau
+                INNER JOIN emblem e ON h.emblem_id = e.id_emblem
+                WHERE e.id_emblem = :id";
 
             return $this->getMultipleResults(
                     DAO::select($sql,['id'=>$id]),
