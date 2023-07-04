5974e42e0a6b606c9e8888cb9b7252d2c2ca26c1
KrouriB <brice.krouri@gmail.com>
2023-07-04 22:36:53 +0200

    display of number of ring per caracter

--- view/show/ringByEmblem.php
+++ view/show/ringByEmblem.php
@@ -1,9 +1,22 @@
 <?php
+
     $qtts = $result["data"]['qtt'];
     $emblem = $result["data"]['emblem'];
 
-    foreach($emblem as $a){
-        echo "<h1>".$a->getPerso()."</h1>";
+    foreach($emblem as $title){
+        echo "<h1>".$title->getPerso()."</h1>";
     }
-?>
 
+?>
+<div id="container">
+    <?php
+        foreach($qtts as $qtt){ ?>
+                <div class="aSingleRing twelve">
+                    <h4><?= $qtt->getHeros()->getNom() ?> <?= $qtt->getAnneau()->getLettre() ?></h4>
+                    <a href="#">-</a>
+                    <span><?= $qtt->getQtt() ?></span>
+                    <a href="#">+</a>
+                </div>
+            <?php }
+    ?>
+</div>
---
+++ view/show/ringByRarity.php
@@ -0,0 +1,17 @@
+<?php
+
+    $qtts = $result["data"]['qtt'];
+
+?>
+<div id="container">
+    <?php
+        foreach($qtts as $qtt){ ?>
+                <div class="aSingleRing ten">
+                    <h4><?= $qtt->getHeros()->getNom() ?> <?= $qtt->getAnneau()->getLettre() ?></h4>
+                    <a href="#">-</a>
+                    <span><?= $qtt->getQtt() ?></span>
+                    <a href="#">+</a>
+                </div>
+            <?php }
+    ?>
+</div>
