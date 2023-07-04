ecf0272c1731cbd01537c5c521d97c6adfd04f04
KrouriB <brice.krouri@gmail.com>
2023-07-04 23:06:37 +0200

    link to update the db

--- view/show/ringByEmblem.php
+++ view/show/ringByEmblem.php
@@ -13,9 +13,9 @@
         foreach($qtts as $qtt){ ?>
                 <div class="aSingleRing twelve">
                     <h4><?= $qtt->getHeros()->getNom() ?> <?= $qtt->getAnneau()->getLettre() ?></h4>
-                    <a href="#">-</a>
+                    <a href="index.php?ctrl=show&action=less&id=<?= $qtt->getHeros()->getId() ?>">-</a>
                     <span><?= $qtt->getQtt() ?></span>
-                    <a href="#">+</a>
+                    <a href="index.php?ctrl=show&action=more&id=<?= $qtt->getHeros()->getId() ?>">+</a>
                 </div>
             <?php }
     ?>
